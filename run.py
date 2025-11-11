eac8bcbb776410314a2620575fb4cbc105fc5773
Esha Susan Shaji <eshasusanshaji@gmail.com>
2025-11-11 15:07:34 +0530

    Add logic for opening webcam with opencv

---
+++ run.py
@@ -0,0 +1,25 @@
+import cv2
+from detector import detect_face
+def main():
+    cv2.namedWindow("AURA")
+    cap=cv2.VideoCapture(0)
+
+    if cap.isOpened():
+        rval,frame=cap.read()
+    else:
+        rval=False
+        print("Error! Couldn't open the webcam")
+
+
+    while rval:
+        frame=detect_face(frame)
+        cv2.imshow("AURA",frame)
+        rval,frame=cap.read()
+        key=cv2.waitKey(20)
+        if key==27:
+            break
+    cv2.destroyWindow("AURA")
+    cap.release()
+
+if __name__=="__main__":
+    main()
